76b45c7b53611d8cd56a20135966f80a5d9cf406
Chakib_Chemso <chemsoblakblak@gmail.com>
2023-04-22 04:26:58 +0100

    tutorial 04
    https://www.youtube.com/watch?v=ecMcXW6MSYU
    Signed-off-by: Chakib_Chemso <chemsoblakblak@gmail.com>

--- Vulkan Engine/vk_pipeline.hpp
+++ Vulkan Engine/vk_pipeline.hpp
@@ -8,6 +8,18 @@ namespace vk_engine
 {
     struct pipeline_config_info
     {
+        VkViewport viewport{};
+        VkRect2D scissor{};
+        VkPipelineViewportStateCreateInfo viewport_info{};
+        VkPipelineInputAssemblyStateCreateInfo input_assembly_info{};
+        VkPipelineRasterizationStateCreateInfo rasterization_info{};
+        VkPipelineMultisampleStateCreateInfo multisample_info{};
+        VkPipelineColorBlendAttachmentState color_blend_attachment{};
+        VkPipelineColorBlendStateCreateInfo color_blend_info{};
+        VkPipelineDepthStencilStateCreateInfo depth_stencil_info{};
+        VkPipelineLayout pipeline_layout = nullptr;
+        VkRenderPass render_pass = nullptr;
+        uint32_t sub_pass = 0;
     };
 
     class vk_pipeline
@@ -19,7 +31,7 @@ namespace vk_engine
             const std::string& frag_shader_path,
             const pipeline_config_info& config_info);
 
-        ~vk_pipeline() = default;
+        ~vk_pipeline();
 
         vk_pipeline(const vk_pipeline&) = delete;
         void operator=(const vk_pipeline&) = delete;
@@ -29,16 +41,18 @@ namespace vk_engine
     private:
         static std::vector<char> read_file(const std::string& file_path);
 
-        static void create_graphics_pipeline(
+        void create_graphics_pipeline(
             const std::string& vert_shader_path,
             const std::string& frag_shader_path,
             const pipeline_config_info& config_info);
 
-        void create_shader_module(const std::vector<char>& code, VkShaderModule* shader_module);
+        void create_shader_module(const std::vector<char>& code, VkShaderModule* shader_module) const;
 
         vk_device& vk_device_;
-        VkPipeline graphics_pipeline_{};
-        VkShaderModule vert_shader_module_{};
-        VkShaderModule frag_shader_module_{};
+        
+        VkPipeline graphics_pipeline_;
+        
+        VkShaderModule vert_shader_module_;
+        VkShaderModule frag_shader_module_;
     };
 }
